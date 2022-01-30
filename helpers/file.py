4b6f825cbaf31ffcddded69ebe29183baa56f07d
VVX7 <info@vvx7.io>
2022-01-30 10:23:20 -0500

    init

---
+++ helpers/file.py
@@ -0,0 +1,67 @@
+import json
+import os
+from stix2 import Bundle
+import stix2
+
+
+def write_disarm_dir(dir):
+    """
+
+     Args:
+        dir (str): a directory name
+
+    Returns:
+
+    """
+    try:
+        os.mkdir('output/DISARM')
+    except FileExistsError:
+        pass
+
+    try:
+        os.mkdir('output/DISARM/' + dir)
+    except FileExistsError:
+        pass
+
+
+def write_file(file_name, file_data):
+    """Write a JSON file to ./output.
+
+    Args:
+        file_name (str): a file name
+        file_data (str): the file json data
+
+    Returns:
+
+    """
+    with open(file_name, 'w') as f:
+        # f.write(json.dumps(file_data, sort_keys=True, indent=4))
+        f.write(file_data.serialize(pretty=True))
+        f.write('\n')
+
+
+def write_files(stix_objects):
+    """
+
+    Args:
+        stix_objects:
+
+    Returns:
+
+    """
+    for i in stix_objects:
+        write_disarm_dir(i.type)
+        write_file(f"output/DISARM/{i.type}/{i.id}.json", Bundle(i, allow_custom=True))
+
+
+def write_bundle(bundle, bundle_name):
+    """
+
+    Args:
+        bundle:
+        bundle_name:
+
+    Returns:
+
+    """
+    write_file(f"output/DISARM/{bundle_name}.json", bundle)
